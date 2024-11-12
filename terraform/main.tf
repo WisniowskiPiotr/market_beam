4a0876ce1bdc0a61793d2082adb5fc556c2ef50f
Wisniowski Piotr <pwisniowski@mdsol.com>
2024-11-12 23:23:13 +0100

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,17 +15,37 @@ resource "terraform_data" "minikube_cluster" {
   }
 
   provisioner "local-exec" {
-    when = destroy
+    when    = destroy
     command = "minikube stop && minikube delete"
   }
 }
 
+locals {
+  host_data_path     = "../data/pulsar"
+  minikube_data_path = "/data/pulsar"
+}
+
+resource "terraform_data" "minikube_host_mount" {
+  # this needs to stay alive constantly so we cannon use it in depends_on
+  provisioner "local-exec" {
+    command = "mkdir -p ${local.host_data_path} && minikube mount ${local.host_data_path}:${local.minikube_data_path}"
+  }
+  depends_on = [terraform_data.minikube_cluster]
+}
+
+
 resource "helm_release" "pulsar" {
-  name       = "pulsar"
-  repository = "https://pulsar.apache.org/charts"
-  chart      = "pulsar"
-  #namespace  = "pulsar-namespace"
-  version    = "3.7.0"
-  values     = [file("pulsar-values.yaml")]
-  depends_on = [ terraform_data.minikube_cluster ]
+  repository       = "https://pulsar.apache.org/charts"
+  chart            = "pulsar"
+  version          = "3.7.0"
+  name             = "pulsar"
+  namespace        = "pulsar"
+  create_namespace = true
+  values           = [file("pulsar-values.yaml")]
+  timeout          = 15 * 60
+  set {
+    name  = "bookkeeper.storage.hostPath.path"
+    value = local.minikube_data_path
+  }
+  depends_on = [terraform_data.minikube_cluster]
 }
