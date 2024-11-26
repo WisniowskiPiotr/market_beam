8388737cf4a98e1de55a2a90c80a3d676008f776
Wisniowski Piotr <pwisniowski@mdsol.com>
2024-11-26 22:42:35 +0100

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,26 @@ provider "helm" {
     config_path = "~/.kube/config"
   }
 }
-# TODO: run instalations of soft as part of the terraform script?
+
+locals {
+  host_data_path     = abspath("../data/pulsar")
+  minikube_data_path = "/data/pulsar"
+  volume_count = 4 # todo: specify volumes as set instead and
+}
+
+resource "terraform_data" "minikube_host_mount_folder" {
+  count = local.volume_count
+  provisioner "local-exec" {
+    command = "mkdir -p ${local.host_data_path}/volume_${count.index}"
+  }
+}
 
 resource "terraform_data" "minikube_cluster" {
+  depends_on = [ terraform_data.minikube_host_mount_folder ]
   provisioner "local-exec" {
-    command = "minikube start --memory=8192 --cpus=12"
+    command = "minikube start --memory=8192 --cpus=12 --mount-string='${local.host_data_path}:${local.minikube_data_path}' --mount"
+    # --gpus='all'
+    # --mount=true
   }
 
   provisioner "local-exec" {
@@ -20,17 +35,26 @@ resource "terraform_data" "minikube_cluster" {
   }
 }
 
-locals {
-  host_data_path     = "../data/pulsar"
-  minikube_data_path = "/data/pulsar"
-}
-
-resource "terraform_data" "minikube_host_mount" {
-  # this needs to stay alive constantly so we cannon use it in depends_on
-  provisioner "local-exec" {
-    command = "mkdir -p ${local.host_data_path} && minikube mount ${local.host_data_path}:${local.minikube_data_path}"
+resource "kubernetes_persistent_volume" "persistent_volume" {
+  count = local.volume_count
+  depends_on = [ terraform_data.minikube_host_mount_folder ]
+  metadata {
+    name = "persistent-volume-${count.index}"
   }
-  depends_on = [terraform_data.minikube_cluster]
+
+  spec {
+    capacity = {
+      storage = "2Gi"
+    }
+
+    access_modes = ["ReadWriteOnce"]
+
+    persistent_volume_source {
+      host_path {
+        path = "${local.minikube_data_path}/volume_${count.index}" 
+      }
+    }
+  }
 }
 
 
